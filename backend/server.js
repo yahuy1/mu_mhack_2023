bf5e74f8bd1632bd47081aa4ad47b8aa5ae41166
yahuy1 <nguyengiahuyak29@gmail.com>
2023-02-17 22:44:48 -0500

    schemas + mongo connection

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv").config()
+const cors = require("cors")
+
+const app = express()
+app.use(cors({origin: true}))
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+const PORT = process.env.PORT || 8080
+
+app.get("/", (req, res) => {
+    res.send("Xin chao cac ban nho be!")
+})
+
+
+mongoose.set('strictQuery', false);
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server Running on port ${PORT}`)
+        })
+    })
+    .catch((err) => console.log(err))

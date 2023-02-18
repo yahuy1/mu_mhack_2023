16647e834d741d97e0981e8159746be4826f57e1
duong-vo <vodh2@miamioh.edu>
2023-02-17 23:50:44 -0500

    handle create new user and add user authentication

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,18 @@ const dotenv = require("dotenv").config()
 const cors = require("cors")
 
 const app = express()
+const User = require("./routes/User")
 app.use(cors({origin: true}))
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-    res.send("Xin chao cac ban nho be!")
+    res.send("Xin chao cac ban nho be!");
 })
+
+app.use("/user", User);
 
 
 mongoose.set('strictQuery', false);
@@ -20,7 +23,7 @@ mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(PORT, () => {
-            console.log(`Server Running on port ${PORT}`)
+            console.log(`Server Running on port ${PORT}`);
         })
     })
-    .catch((err) => console.log(err))
+    .catch((err) => console.log(err));

1c1bea901f37e9b4fbae78e51085842f6606d353
Lamp-Nguyen <lamnguyenmai02@gmail.com>
2023-02-18 04:21:02 -0500

    Updated login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.css'
 import Feed from './Components/Feed/Feed.js'
+import LogIn from './Components/LogIn/LogIn';
 import Sign_Up from './Components/Sign_Up/Sign_Up.js'
 
 function App() {
   return (
     <div className="App">
-      <Feed/>
+      <LogIn/>
     </div>
   );
 }
